5b8893bea7999bfc856fddeb95fe85be435b722e
Javier Zapata <javierzapata82@gmail.com>
2021-02-06 17:20:33 +0100

    Migrate to TypeScript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
 import Date from '../components/date';
@@ -5,14 +6,14 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async (context) => {
   const allPostsData = getSortedPostsData();
   return {
     props: {
       allPostsData,
     },
   };
-}
+};
 
 export default function Home({ allPostsData }) {
   return (
